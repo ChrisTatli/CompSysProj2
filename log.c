bc06afe0dac9b30c7fee1b177624b830a3fd7e73
Christopher Tatli <christopher.tatli@gmail.com>
2017-05-14 19:44:58 +1000

    added function to log time of server interactions

--- log.c
+++ log.c
@@ -5,9 +5,13 @@
 
 #include "log.h"
 
+#define TIME_BUFFER 26
+
 FILE *log_f = NULL;
 pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
 
+/* Fucntion Declarations*/
+void log_datetime();
 
 
 /*
@@ -24,10 +28,26 @@ void init_log(char *filepath){
          fprintf(stderr, "Error opening file %s\n", filepath);
       } else {
          log_f = file;
-         fprintf(log_f, "Server Initialised" );
+         log_datetime();
+         fprintf(log_f, " Server Initialised" );
       }
    } else {
       fprintf(stderr, "File %s is already open\n",filepath);
    }
    pthread_mutex_unlock(&lock);
+}
+
+/*
+** Logs the date and time that a client/server action takes place
+*/
+void log_datetime(){
+   // Based on code from : http://bit.ly/2qiYlIO
+   time_t datetime;
+   char buffer[TIME_BUFFER];
+   struct tm* tm_info;
+
+   time(&datetime);
+   tm_info = localtime(&datetime);
+   strftime(buffer,TIME_BUFFER,"%Y-%m-%d %H:%M:%S",tm_info);
+   fprintf(log_f, "[%s]",buffer);
 }
